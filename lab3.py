4015ce0148e1f842eaf00786c09c35bc34f73bfa
Виктория Журавлёва <papilata05@gmail.com>
2025-10-31 18:07:28 +0700

    форма

--- lab3.py
+++ lab3.py
@@ -1,12 +1,12 @@
-
 from flask import Blueprint, render_template, request, make_response, redirect
+
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab33():
     name = request.cookies.get('name')
     name_color = request.cookies.get('name_color')
-    return render_template('/lab3/lab3/lab3.html', name=name, name_color=name_color)
+    return render_template('lab3/lab3.html', name=name, name_color=name_color)
 
 @lab3.route('/lab3/cookie')
 def cookie():
@@ -23,3 +23,10 @@ def clear_cookies():
     resp.set_cookie('age', '', expires=0)
     resp.set_cookie('name_color', '', expires=0)
     return resp
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex)

18f7474ae033b1441b4b7eda858d9dc09acbabe1
Виктория Журавлёва <papilata05@gmail.com>
2025-10-31 18:47:12 +0700

    валидация

--- lab3.py
+++ lab3.py
@@ -26,7 +26,11 @@ def clear_cookies():
 
 @lab3.route('/lab3/form1')
 def form1():
+    errors = {}
     user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заолните поле!'
+
     age = request.args.get('age')
     sex = request.args.get('sex')
-    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)

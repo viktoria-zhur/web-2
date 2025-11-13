1877bb99392a3c2de931342199ce249e767437c6
Виктория Журавлёва <papilata05@gmail.com>
2025-11-13 23:16:27 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -133,3 +133,17 @@ def tree():
     
     # Редирект на эту же страницу методом GET
     return redirect('/lab4/tree')
+
+@lab4.route('/lab4/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab4/login.html', authorized=False)
+
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login == 'alex' and password == '123':
+        return render_template('lab4/login.html', login=login, authorized=True)
+
+    error = 'Неверные логин и/или пароль'
+    return render_template('lab4/login.html', error=error, authorized=False)

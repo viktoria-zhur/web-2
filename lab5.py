e2b4d773a4361bae88d29374aeb6dec33d8cf2de
Виктория Журавлёва <papilata05@gmail.com>
2025-11-26 12:03:11 +0700

    Аутентификация

--- lab5.py
+++ lab5.py
@@ -1,15 +1,12 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
 import psycopg2
 
 lab5 = Blueprint('lab5', __name__)
 
 @lab5.route('/lab5')
 def main():
-    return render_template('lab5/lab5.html', username="anonymous")
-
-@lab5.route('/lab5/login')
-def login():
-    return "форма авторизации"
+    username = session.get('username', 'Anonymous')
+    return render_template('lab5/lab5.html', username=username)
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
@@ -50,6 +47,49 @@ def register():
     
     except Exception as e:
         return render_template('lab5/register.html', error=f'Ошибка базы данных: {str(e)}')
+
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login and password):
+        return render_template('lab5/login.html', error="Заполните все поля")
+    
+    try:
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='viktoria_zhuravleva_knowledge_base',
+            user='viktoria_zhuravleva_knowledge_base',
+            password='123'
+        )
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM users WHERE login = %s;", (login,))
+        user = cur.fetchone()
+
+        if not user:
+            cur.close()
+            conn.close()
+            return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+
+        if user[2] != password:
+            cur.close()
+            conn.close()
+            return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+        
+        session['username'] = login
+        
+        cur.close()
+        conn.close()
+        return render_template('lab5/success_login.html', login=login)
+    
+    except Exception as e:
+        return render_template('lab5/login.html', error=f'Ошибка базы данных: {str(e)}')
+
 @lab5.route('/lab5/list')
 def list_articles():
     return "список статей"
